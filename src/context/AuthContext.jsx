cc7ac458ad6ac813a339f834d7203401b7f71978
Navaf V <navafkcp@gmail.com>
2025-11-19 00:31:54 +0530

    implementing

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -79,7 +79,7 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={{ user, login, logout, loading, isAdmin: user?.is_staff }}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   );
 };
